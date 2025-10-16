029c526d39e34188d674ef97341405ddc4c0b053
Yahya Ehsan <124023463+yussypu@users.noreply.github.com>
2025-10-16 17:06:15 +0200

    Add FooterMinimal component

---
+++ src/components/FooterMinimal.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function FooterMinimal(){
+  return (
+    <footer className="mx-auto max-w-5xl px-4 py-8 text-xs opacity-70 flex items-center justify-between">
+      <div>About • Keyboard shortcuts</div>
+      <div className="opacity-70"><a href="#" className="underline">Support</a></div>
+    </footer>
+  );
+}
